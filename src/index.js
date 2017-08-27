9f9fca32a8d265730782ff264d4f24147780f436
Kevin Pavao <kpavao84@gmail.com>
2017-08-27 16:19:42 -0500

    Turn everything from classes to functions

--- src/index.js
+++ src/index.js
@@ -3,69 +3,59 @@ import ReactDOM from 'react-dom';
 
 const MockWeather = require('./mock-response.json');
 
-class Weather extends React.Component {
-  render() {
-    return (
-      <div className="weather">
-        <h1>Weather</h1>
-        <CurrentWeather weather={this.props.weather.currently} />
-        <MinutelyWeather weather={this.props.weather.minutely} />
-        <HourlyWeather weather={this.props.weather.hourly} />
-        <DailyWeather weather={this.props.weather.daily} />
-      </div>
-    );
-  }
+function Weather(props) {
+  return (
+    <div className="weather">
+      <h1>Weather</h1>
+      <CurrentWeather weather={props.weather.currently} />
+      <MinutelyWeather weather={props.weather.minutely} />
+      <HourlyWeather weather={props.weather.hourly} />
+      <DailyWeather weather={props.weather.daily} />
+    </div>
+  );
 }
 
-class CurrentWeather extends React.Component {
-  render() {
-    return (
-      <div className="currentWeather">
-        <p>
-          {this.props.weather.summary} -- {this.props.weather.temperature}
-        </p>
-      </div>
-    );
-  }
+function CurrentWeather(props) {
+  return (
+    <div className="currentWeather">
+      <p>
+        {props.weather.summary} -- {props.weather.temperature}
+      </p>
+    </div>
+  );
 }
 
-class MinutelyWeather extends React.Component {
-  render() {
-    return (
-      <div className="minutelyWeather">
-        <p>
-          {this.props.weather.summary}
-        </p>
-      </div>
-    );
-  }
+function MinutelyWeather(props) {
+  return (
+    <div className="minutelyWeather">
+      <p>
+        {props.weather.summary}
+      </p>
+    </div>
+  );
 }
 
-class HourlyWeather extends React.Component {
-  render() {
-    return (
-      <div className="hourlyWeather">
-        <p>
-          {this.props.weather.summary}
-        </p>
-      </div>
-    );
-  }
+function HourlyWeather(props) {
+  return (
+    <div className="hourlyWeather">
+      <p>
+        {props.weather.summary}
+      </p>
+    </div>
+  );
 }
 
-class DailyWeather extends React.Component {
-  render() {
-    return (
-      <div className="dailyWeather">
-        <p>
-          {this.props.weather.summary}
-        </p>
-      </div>
-    );
-  }
+function DailyWeather(props) {
+  return (
+    <div className="dailyWeather">
+      <p>
+        {props.weather.summary}
+      </p>
+    </div>
+  );
 }
 
 ReactDOM.render(
-  <Weather weather={MockWeather}/>,
+  <Weather weather={MockWeather} />,
   document.getElementById('root'),
 );
