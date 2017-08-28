5533f8a7099c7f0830cc312c6250fa77e1d3ba96
Kevin Pavao <kpavao84@gmail.com>
2017-08-27 23:55:24 -0500

    Made all components stateless functional

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,55 +1,45 @@
 import React from 'react';
 
-function Weather(props) {
-  return (
-    <div className="weather">
-      <h1>Weather</h1>
-      <CurrentWeather weather={props.weather.currently} />
-      <MinutelyWeather weather={props.weather.minutely} />
-      <HourlyWeather weather={props.weather.hourly} />
-      <DailyWeather weather={props.weather.daily} />
-    </div>
-  );
-}
+const Weather = (props => (
+  <div className="weather">
+    <h1>Weather</h1>
+    <CurrentWeather weather={props.weather.currently} />
+    <MinutelyWeather weather={props.weather.minutely} />
+    <HourlyWeather weather={props.weather.hourly} />
+    <DailyWeather weather={props.weather.daily} />
+  </div>
+));
 
-function CurrentWeather(props) {
-  return (
-    <div className="currentWeather">
-      <p>
-        {props.weather.summary} -- {props.weather.temperature}
-      </p>
-    </div>
-  );
-}
+const CurrentWeather = (props => (
+  <div className="currentWeather">
+    <p>
+      {props.weather.summary} -- {props.weather.temperature}
+    </p>
+  </div>
+));
 
-function MinutelyWeather(props) {
-  return (
-    <div className="minutelyWeather">
-      <p>
-        {props.weather.summary}
-      </p>
-    </div>
-  );
-}
+const MinutelyWeather = (props => (
+  <div className="minutelyWeather">
+    <p>
+      {props.weather.summary}
+    </p>
+  </div>
+));
 
-function HourlyWeather(props) {
-  return (
-    <div className="hourlyWeather">
-      <p>
-        {props.weather.summary}
-      </p>
-    </div>
-  );
-}
+const HourlyWeather = (props => (
+  <div className="hourlyWeather">
+    <p>
+      {props.weather.summary}
+    </p>
+  </div>
+));
 
-function DailyWeather(props) {
-  return (
-    <div className="dailyWeather">
-      <p>
-        {props.weather.summary}
-      </p>
-    </div>
-  );
-}
+const DailyWeather = (props => (
+  <div className="dailyWeather">
+    <p>
+      {props.weather.summary}
+    </p>
+  </div>
+));
 
 export default Weather;
