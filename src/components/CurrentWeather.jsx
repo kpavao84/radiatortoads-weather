ddbc659788f6fb28bad04c9329ed4846f180c0d5
Kevin Pavao <kpavao84@gmail.com>
2017-08-28 22:21:37 -0500

    Add a better degree symbol

--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const CurrentWeather = (props => (
   <div className="currentWeather">
     <p>
-      {props.icon} {props.temperature}˚ {props.summary}
+      {props.icon} {props.temperature}° {props.summary}
     </p>
   </div>
 ));
