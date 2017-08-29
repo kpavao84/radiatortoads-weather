3ce51111510f997c52964302b0a7134fbf0b087d
Kevin Pavao <kpavao84@gmail.com>
2017-08-28 23:47:43 -0500

    Put icon in a separate component

--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -3,9 +3,22 @@ import ReactSkycons from './ReactSkycons';
 
 const CurrentWeather = (props => (
   <div className="currentWeather">
-    <ReactSkycons icon={props.icon.toUpperCase().replace('-', '_')} color="black" />
+    <CurrentIcon icon={props.icon} />
     {props.temperature}° {props.summary}
   </div>
 ));
+
+const CurrentIcon = (props => (
+  <div style={iconStyle} className="currentIcon">
+    <ReactSkycons icon={props.icon.toUpperCase().replace('-', '_')} color="black" />
+  </div>
+));
+
+const iconStyle = {
+  width: '15%',
+  height: '15%',
+};
+
+
 
 export default CurrentWeather;
